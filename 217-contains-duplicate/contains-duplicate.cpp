bf22d8183ce2e319d952482439c230913f720621
Divyesh Jethava <jethavadivyesh679@gmail.com>
2025-06-18 19:19:31 +0530

    Time: 67 ms (21.47%) | Memory: 93.1 MB (10.20%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int n : nums){
+            mp[n]++;
+        }
+        for(const auto &pair : mp){
+            if(pair.second > 1){
+                return true;
+            }
+        }
+     return false;
+    }
+};
